523c8b130ba3833d1e854e16464764df02688c5a
syno <nogary11@gmail.com>
2020-05-02 15:38:53 +0900

    posts 게시판기능중 조회, 등록, 수정, 테스트 코드 작성 및 실 코드 작성함.

---
+++ src/main/java/dev/syno/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package dev.syno.book.springboot.web.dto;
+
+import dev.syno.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+
+}
