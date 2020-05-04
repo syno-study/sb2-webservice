7e46e184bad9dd93bf37635cc091be5a54d1a76f
syno <nogary11@gmail.com>
2020-05-04 17:33:36 +0900

    테스트 코드 오류 해결-> 시큐리티 적용으로인한 오류.

--- src/main/java/dev/syno/book/springboot/Application.java
+++ src/main/java/dev/syno/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing 테스트 불가 제거함.
 @SpringBootApplication
 public class Application {
 
