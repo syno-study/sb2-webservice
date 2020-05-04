74f1e93702a4bf378343ea410ef2ce1f1d3574ec
syno <nogary11@gmail.com>
2020-05-04 10:48:19 +0900

    스프링 시큐리티 이용하여 oauth2 구현 -> 구글 로그인 기능 구현

--- src/main/java/dev/syno/book/springboot/web/IndexController.java
+++ src/main/java/dev/syno/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package dev.syno.book.springboot.web;
 
+import dev.syno.book.springboot.config.auth.LoginUser;
+import dev.syno.book.springboot.config.auth.dto.SessionUser;
 import dev.syno.book.springboot.service.posts.PostsService;
 import dev.syno.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findByDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
