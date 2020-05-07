8a0dfcd8ceba150a062827db17f4c6a05a1a7c61
syno-study <nosoo11@naver.com>
2020-05-07 17:30:05 +0900

    무중단 배포를 위한 실행 스크립트 몇개 추가 중지, 시작, 포트체크, 스위치

--- script/profile.sh
+++ script/profile.sh
@@ -2,7 +2,7 @@
 
 # 쉬고 있는 프로파일 찾기 : real1이 사용중이면real2가쉬고있고, 반대면 real1이 쉬고있음
 
-function find_idel_profile() {
+function find_idle_port() {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] #400보다 크면 에러포함
