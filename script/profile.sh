665fb70f167447987ef56bf87d9538e237d83a09
syno-study <nosoo11@naver.com>
2020-05-07 17:31:06 +0900

    무중단 배포를 위한 실행 스크립트 몇개 추가 중지, 시작, 포트체크, 스위치

--- script/profile.sh
+++ script/profile.sh
@@ -26,7 +26,7 @@ function find_idle_port() {
 function find_idle_port() {
   IDLE_PROFILE=$(find_idel_profile)
 
-  if[ ${IDLE_PROFILE} == real1 ]
+  if [ ${IDLE_PROFILE} == real1 ]
   then
     echo "8081"
   else
