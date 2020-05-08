fa6bd02958851d8bbdbab0c30040758ea5b6acd3
syno-study <nosoo11@naver.com>
2020-05-08 12:57:10 +0900

    무중단 배포를 위한 실행 스크립트 몇개 추가 중지, 시작, 포트체크, 스위치

--- script/start.sh
+++ script/start.sh
@@ -16,7 +16,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/zip/ |grep *.jar | tail -n 1)
 echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
+chmod +x $REPOSITORY/$JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
