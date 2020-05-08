4cbf3d444ae3d0fdc68286ccf76465ae73446f14
syno-study <nosoo11@naver.com>
2020-05-08 14:02:58 +0900

    무중단 배포를 위한 실행 스크립트 몇개 추가 중지, 시작, 포트체크, 스위치

--- script/start.sh
+++ script/start.sh
@@ -7,11 +7,12 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot2-webservice
 
-echo "> Build 파일복사"
+echo "> Build 파일복사 $REPOSITORY/zip/*.jar $REPOSITORY/"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/zip/ |grep *.jar | tail -n 1)
+
+JAR_NAME=$(ls -tr $REPOSITORY/zip/ | grep *.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
 
