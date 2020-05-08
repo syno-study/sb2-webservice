1ccb5bfd90438509b1c12c5424175275f0b71e60
syno-study <nosoo11@naver.com>
2020-05-08 14:40:10 +0900

    무중단 배포를 위한 실행 스크립트 몇개 추가 중지, 시작, 포트체크, 스위치

--- script/start.sh
+++ script/start.sh
@@ -26,5 +26,5 @@ IDEL_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDEL_PROFILE 로 실행합니다."
 nohup java -jar \
 -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDEL_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml  \
--Dspring.profiles.active=real  \
+-Dspring.profiles.active=$IDEL_PROFILE  \
 $JAR_NAME > $REPOSITORY/nohup.out  2>&1 &
